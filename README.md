bf595141e03792d04346dcd1d7587bb39732a66a
richardmillson <8217613+richardmillson@users.noreply.github.com>
2016-05-04 21:44:33 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+calendar
+============
+[![Build Status](https://travis-ci.org/richardmillson/galois.svg)](https://travis-ci.org/richardmillson/galois)
+
+Enter a month and year and get a pdf calendar built from latex
---
+++ generate_calendar.py
@@ -0,0 +1,8 @@
+import calendar
+
+c = calendar.Calendar()
+c.setfirstweekday(3)
+c.prmonth(2016, 5)
+
+# def generate_calendar(month, year):
+	""" given a month and year, will create a pdf calendar for that month """
