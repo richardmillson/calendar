4e0967c5ef0745e26da3808105088e234a14d08a
richardmillson <8217613+richardmillson@users.noreply.github.com>
2016-05-05 22:23:38 -0400

    Iterate over days in month

--- generate_calendar.py
+++ generate_calendar.py
@@ -1,8 +1,17 @@
 import calendar
 
+# c = calendar.TextCalendar()
+# c.setfirstweekday(5)
+# c.prmonth(2016, 5)
+
 c = calendar.Calendar()
-c.setfirstweekday(3)
-c.prmonth(2016, 5)
+c.setfirstweekday(5)
+for day in c.itermonthdates(2016, 5):
+    print day
+for day in c.itermonthdays2(2016, 5):
+    print day
+for day in c.itermonthdays(2016, 5):
+    print day
 
 # def generate_calendar(month, year):
-	""" given a month and year, will create a pdf calendar for that month """
+# 	""" given a month and year, will create a pdf calendar for that month """
